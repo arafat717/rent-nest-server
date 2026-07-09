1ec856f0185a2ee376af1e5c0981b2b639e14082
arafat717 <arafatjibon33@gmail.com>
2026-07-09 20:29:35 +0600

    confirm api payment modify

--- src/modules/payment/payment.route.ts
+++ src/modules/payment/payment.route.ts
@@ -5,16 +5,9 @@ import { Role } from "../../../generated/prisma/enums";
 
 const router = Router();
 
-// POST /api/payments/create
 router.post("/create", auth(Role.TENANT), paymentController.createPayment);
-
-// POST /api/payments/confirm
 router.post("/confirm", auth(Role.TENANT), paymentController.confirmPayment);
-
-// GET /api/payments
 router.get("/", auth(Role.TENANT), paymentController.getMyPayments);
-
-// GET /api/payments/:id
 router.get(
   "/:id",
   auth(Role.TENANT, Role.LANDLORD, Role.ADMIN),
