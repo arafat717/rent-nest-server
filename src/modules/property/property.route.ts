bd5bc5817be2581bd910553b1e5a4ad1da53d9f8
arafat717 <arafatjibon33@gmail.com>
2026-07-09 20:34:40 +0600

    fix property issue

--- src/modules/property/property.route.ts
+++ src/modules/property/property.route.ts
@@ -5,13 +5,10 @@ import { Role } from "../../../generated/prisma/enums";
 
 const router = Router();
 
-// GET /api/properties - public, with filters
 router.get("/", propertyController.getAllProperties);
 router.post("/", auth(Role.LANDLORD), propertyController.createProperty);
 router.put("/:id", auth(Role.LANDLORD), propertyController.updateProperty);
 router.delete("/:id", auth(Role.LANDLORD), propertyController.deleteProperty);
-
-// GET /api/properties/:id - public
 router.get("/:id", propertyController.getSingleProperty);
 
 export const propertyRoute = router;
